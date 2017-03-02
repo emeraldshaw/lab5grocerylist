fc79653f02e58b7869e2f1b63c03b9dc81fd8764
Emerald <eshaw589@gmail.com>
2017-03-01 20:33:01 -0500

    initial commit

---
+++ lab13/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  $.get("https://www.reddit.com/r/aww/.json", function(stuff){
+
+    var redditArray = stuff.data.children;
+    for(var i=0; i<redditArray.length; i++){
+
+      var thumbnail = redditArray[i].data.thumbnail;
+      var title = redditArray[i].data.title;
+      var author = redditArray[i].data.author;
+      var score = redditArray[i].data.score;
+      var link = "https://www.reddit.com" + redditArray[i].data.permalink;
+
+      $('section').append('<div class = "post"><h2>' + title + '</h2>'
+      + author + '<br>' + score +
+      '<a href="' + link + '"/><img src="' + thumbnail + '"/></a></div>');
+
+    }
+
+  });
+
+});
